6d6714a51af17e8b94f1a4fc81bae71027385afa
Gerson Severo <gersonsevero90@gmail.com>
2022-02-21 10:46:46 +0000

    Data adicionada
    Data para conter todas as informações importantes em relação ao usuário, bem como a contagem de seguidores

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,19 @@
 <script>
 export default {
   name: "App",
+  data() {
+    return {
+      followers: 0,
+      user: {
+        id: 1,
+        username: "GersonSevero",
+        firstName: "Gerson",
+        lastName: "Severo",
+        email: "gersonsevero90@gmail.com",
+        isAdmin: true,
+      },
+    };
+  },
 };
 </script>
 

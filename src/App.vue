3b28b069d05edb561e4c6c36dc515e6fd5562d5f
Gerson Severo <gersonsevero90@gmail.com>
2022-02-21 19:34:32 +0000

    Refactoring
    Criando um componente apenas para receber a parte de usuario

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,15 @@
 <template>
-  <div id="app">{{ user.username }} - {{ fullName }}</div>
-  <strong>{{ followers }}</strong>
-  <button @click="followUser">Follow</button>
+  <div id="app">
+    <UserProfile />
+  </div>
 </template>
 
 <script>
+import UserProfile from "./components/UserProfile";
+
 export default {
   name: "App",
-  data() {
-    return {
-      followers: 0,
-      user: {
-        id: 1,
-        username: "FoxLegion",
-        firstName: "Gerson",
-        lastName: "Severo",
-        email: "gersonsevero90@gmail.com",
-        isAdmin: true,
-      },
-    };
-  },
-  watch: {
-    followers(newFollowerCount, oldFollowerCount) {
-      if (oldFollowerCount < newFollowerCount) {
-        console.log(`${this.user.username} has gained a follower!`);
-      }
-    },
-  },
-  computed: {
-    fullName() {
-      return `${this.user.firstName} ${this.user.lastName}`;
-    },
-  },
-  methods: {
-    followUser() {
-      this.followers++;
-    },
-  },
-  mounted() {
-    this.followUser();
-  },
+  components: { UserProfile },
 };
 </script>
 
---
+++ src/components/UserProfile.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="user-profile-container">
+    <div class="user-profile_user-panel">
+      <h1 class="user-profile_username">{{ user.username }}</h1>
+      <div class="user-profile_follower-counts">
+        <strong>Followers: </strong> {{ followers }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UserProfile",
+  data() {
+    return {
+      followers: 0,
+      user: {
+        id: 1,
+        username: "FoxLegion",
+        firstName: "Gerson",
+        lastName: "Severo",
+        email: "gersonsevero90@gmail.com",
+        isAdmin: true,
+      },
+    };
+  },
+  watch: {
+    followers(newFollowerCount, oldFollowerCount) {
+      if (oldFollowerCount < newFollowerCount) {
+        console.log(`${this.user.username} has gained a follower!`);
+      }
+    },
+  },
+  computed: {
+    fullName() {
+      return `${this.user.firstName} ${this.user.lastName}`;
+    },
+  },
+  methods: {
+    followUser() {
+      this.followers++;
+    },
+  },
+  mounted() {
+    this.followUser();
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
